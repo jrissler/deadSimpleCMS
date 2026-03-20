1759928a0bbcbb724f25997f5cc331e773326a92
James Rissler <risslerjames@gmail.com>
2026-03-20 11:07:56 -0400

    Fix uploader env.

--- lib/dead_simple_cms_web/uploaders/s3_uploader.ex
+++ lib/dead_simple_cms_web/uploaders/s3_uploader.ex
@@ -9,7 +9,7 @@ defmodule DeadSimpleCms.Uploaders.S3Uploader do
     bucket = bucket()
     key = build_key(original_filename)
     params = %{"Content-Type" => content_type}
-    config = ExAws.Config.new(:s3)
+    config = ex_aws_s3_config()
 
     case S3.presigned_url(config, :put, bucket, key, params: params, expires_in: 3600) do
       {:ok, upload_url} ->
@@ -23,7 +23,11 @@ defmodule DeadSimpleCms.Uploaders.S3Uploader do
   def public_url(bucket, key), do: "https://#{bucket}.s3.amazonaws.com/#{key}"
 
   def bucket do
-    Application.get_env(:dead_simple_cms, :s3, []) |> Keyword.fetch!(:bucket)
+    s3_config() |> Keyword.fetch!(:bucket)
+  end
+
+  def region do
+    s3_config() |> Keyword.fetch!(:region)
   end
 
   @doc """
@@ -36,5 +40,18 @@ defmodule DeadSimpleCms.Uploaders.S3Uploader do
     uuid = Ecto.UUID.generate()
     safe = original_filename |> String.replace(~r/[^a-zA-Z0-9\.\-_]/, "-")
     "uploads/cms_images/#{uuid}/#{safe}"
+  end
+
+  defp ex_aws_s3_config do
+    s3 = s3_config()
+
+    ExAws.Config.new(:s3)
+    |> Map.put(:region, Keyword.fetch!(s3, :region))
+    |> Map.put(:access_key_id, Keyword.fetch!(s3, :access_key_id))
+    |> Map.put(:secret_access_key, Keyword.fetch!(s3, :secret_access_key))
+  end
+
+  defp s3_config do
+    Application.get_env(:dead_simple_cms, :s3, [])
   end
 end
